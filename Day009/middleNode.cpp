70c8f91b124b9f1e8b2535cdf31a4119ae13dce7
xingfukai <xingfukai@cdjrlc.com>
2024-09-09 17:33:03 +0800

    Day009 5th-1 commit

---
+++ Day009/middleNode.cpp
@@ -0,0 +1,57 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+#include <stdlib.h>
+#include <iostream>
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution {
+public:
+    ListNode* middleNode(ListNode* head) {
+
+    }
+};
+
+// 打印链表函数
+void printList(ListNode* head) {
+    ListNode* current = head;
+    while (current != nullptr) {
+        std::cout << current->val;
+        if (current->next != nullptr) {
+            std::cout << " -> ";
+        }
+        current = current->next;
+    }
+    std::cout << std::endl;
+}
+
+// 构建链表函数
+ListNode* createList(std::initializer_list<int> vals) {
+    ListNode* dummy = new ListNode(-1);
+    ListNode* current = dummy;
+    for (int val : vals) {
+        current->next = new ListNode(val);
+        current = current->next;
+    }
+    return dummy->next;
+}
+
+int main() {
+
+    system("pause");
+    return 0;
+}
