629ab83e976585ef18f6527abf5f923994669562
xingfukai <xingfukai@cdjrlc.com>
2024-09-09 17:48:49 +0800

    Day009 5th-2 debug

--- Day009/middleNode.cpp
+++ Day009/middleNode.cpp
@@ -22,7 +22,14 @@ struct ListNode {
 class Solution {
 public:
     ListNode* middleNode(ListNode* head) {
+        ListNode *slow = head;
+        ListNode *fast = head;
 
+        while(fast!=nullptr && fast->next!=nullptr) {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        return slow;
     }
 };
 
@@ -50,7 +57,57 @@ ListNode* createList(std::initializer_list<int> vals) {
     return dummy->next;
 }
 
+void printMiddle(ListNode* middle) {
+    if (middle != nullptr) {
+        std::cout << "Middle node value: " << middle->val << std::endl;
+    } else {
+        std::cout << "List is empty, no middle node." << std::endl;
+    }
+}
+
 int main() {
+    Solution sol;
+
+    // Test case 1: Empty list
+    ListNode* head1 = nullptr;
+    ListNode* middle1 = sol.middleNode(head1);
+    std::cout << "Test case 1 - Empty list: ";
+    printMiddle(middle1);
+
+    // Test case 2: Single node list
+    ListNode* head2 = createList({1});
+    ListNode* middle2 = sol.middleNode(head2);
+    std::cout << "Test case 2 - Single node list: ";
+    printList(head2);
+    printMiddle(middle2);
+
+    // Test case 3: Even number of nodes
+    ListNode* head3 = createList({1, 2, 3, 4});
+    ListNode* middle3 = sol.middleNode(head3);
+    std::cout << "Test case 3 - Even number of nodes: ";
+    printList(head3);
+    printMiddle(middle3);
+
+    // Test case 4: Odd number of nodes
+    ListNode* head4 = createList({1, 2, 3, 4, 5});
+    ListNode* middle4 = sol.middleNode(head4);
+    std::cout << "Test case 4 - Odd number of nodes: ";
+    printList(head4);
+    printMiddle(middle4);
+
+    // Clean up
+    // Function to delete the entire list
+    auto deleteList = [](ListNode* head) {
+        while (head != nullptr) {
+            ListNode* temp = head;
+            head = head->next;
+            delete temp;
+        }
+    };
+
+    deleteList(head2);
+    deleteList(head3);
+    deleteList(head4);
 
     system("pause");
     return 0;
